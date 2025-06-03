40067a34c68e2e476cfcbf08b007952a12a796cc
K R Shrivathsan <shrivathsan24@gmail.com>
2025-06-03 09:00:04 +0530

    Password Security MySQL

--- Backend/util/database.js
+++ Backend/util/database.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     database: 'node-complete',
-    password: 'Shri_2004'
+    password: process.env.DB_PASSWORD
 });
 
 module.exports = pool.promise();
