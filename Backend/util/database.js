04a5a0f37585488c3f0d56bb3bae6a7e4fdeb4d1
K R Shrivathsan <shrivathsan24@gmail.com>
2025-06-03 12:11:09 +0530

    Added Docker and MySql init scripts

--- Backend/util/database.js
+++ Backend/util/database.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
+    host: process.env.DB_HOST || 'localhost',  // 'mysql' inside Docker
+    user: process.env.DB_USER || 'root',
+    database: process.env.DB_NAME || 'node-complete',
     password: process.env.DB_PASSWORD
 });
 
