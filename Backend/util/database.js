528c68bb6cdeda3d2b942cdee1826864c66e7e64
aditya <125145750+aditya07389@users.noreply.github.com>
2025-06-28 11:01:18 +0530

    Update database.js

--- Backend/util/database.js
+++ Backend/util/database.js
@@ -7,7 +7,7 @@ const pool = mysql.createPool({
   database: process.env.DB_NAME,
   port: process.env.DB_PORT || 3306,
   waitForConnections: true,
-  connectionLimit: 5,
+  connectionLimit: 2,
   queueLimit: 0,
 });
 
